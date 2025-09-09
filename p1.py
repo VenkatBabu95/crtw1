0d278ec549e95caababed9d4ec596801eed4074a
VenkatBabu95 <idukudavenkatapathi@gmail.com>
2025-09-09 06:22:51 +0000

    day2

---
+++ p1.py
@@ -0,0 +1,73 @@
+# n=int(input())
+# for i in range(1,n+1):
+#     print(2**i,end=",")
+
+
+# r=65
+# n1=int(input())
+# for i in range(1,n1+1):
+#     print(r,end="")
+#     r+=1
+
+# s=int(input("start:"))
+# n1=int(input("range:"))
+# for i in range(1,n1+1):
+#     print(s+2,end=",")
+#     s+=2
+
+
+# s=int(input("s:"))
+# n=int(input())
+# for i in range(1,n+1):
+#     print(s)
+#     s+=1
+
+
+# n=int(input())
+# for i in range(n):
+#     print("* $ @")
+
+
+# s=65
+# e=90
+# n=int(input())
+# for i in range(n):
+#     print(chr(s),chr(e))
+#     s+=1
+#     e-=1
+
+
+# s=65
+# c=97
+# n=int(input())
+# for i in range(n):
+#     print(chr(c),chr(s))
+#     s+=1
+#     c+=1
+
+# n=int(input())
+# m=int(input())
+# for i in range(n):
+#     for j in range(m):
+#         print("*",end=" ")
+#     print()
+
+
+# n=int(input())
+# m=int(input())
+# for i in range(2*n):
+#     for j in range(1,m+1):
+#         if i%2==0:
+#             print("$ @",end=" ")
+#         else:
+#             print("*",j,end=" ")
+#     print()
+# d=1
+# n=int(input())
+# m=int(input())
+# for i in range(n):
+#     for j in range(m):
+#         print(d,end=" ")
+#         d+=1
+#     print()
+
