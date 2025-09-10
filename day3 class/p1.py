f619ae9371875763fce2d9fcb69ce9fd108e372b
VenkatBabu95 <idukudavenkatapathi@gmail.com>
2025-09-10 06:37:22 +0000

    sw2

--- day3 class/p1.py
+++ day3 class/p1.py
@@ -12,21 +12,39 @@
 #     else:
 #         p+=1
 
-n=int(input())
-for i in range(n-1):
-    for j in range(n-i):
-        print("*",end=" ")
-    for j in range(2*i+1):
-        print("@",end=" ")
-    for j in range(n-i):
-        print("*",end=" ")
-    print()
+# n=int(input())
+# for i in range(n-1):
+#     for j in range(n-i):
+#         print("*",end=" ")
+#     for j in range(2*i+1):
+#         print("@",end=" ")
+#     for j in range(n-i):
+#         print("*",end=" ")
+#     print()
 
-for i in range(n-1,-1,-1):
-    for j in range(n-i):
-        print("*",end=" ")
-    for j in range(2*i+1):
-        print("@",end=" ")
-    for j in range(n-i):
-        print("*",end=" ")
+# for i in range(n-1,-1,-1):
+#     for j in range(n-i):
+#         print("*",end=" ")
+#     for j in range(2*i+1):
+#         print("@",end=" ")
+#     for j in range(n-i):
+#         print("*",end=" ")
+#     print()
+
+# n = 4
+# for i in range(1,n+1):
+#     print(" "*(n-i), "* " * i)
+# for i in range(n-1,0,-1):
+#     print(" "*(n-i), "* " * i)
+n=5
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j<=n-i+1:
+            if (i+j)%2==0:
+                print("1",end=" ")
+            else:
+                print("0",end=" ")
+        else:
+            print(" ",end=" ")
     print()
